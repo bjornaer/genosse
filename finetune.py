31b78cc0341513b8b8c613486ef7b2844c8773b8
bjornaer <maxemijo@gmail.com>
2023-04-21 10:59:58 -0300

    no resume from checkpoint

--- finetune.py
+++ finetune.py
@@ -275,7 +275,8 @@ def train(
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
 
-    trainer.train(resume_from_checkpoint=resume_from_checkpoint)
+    # trainer.train(resume_from_checkpoint=resume_from_checkpoint)
+    trainer.train()
 
     model.save_pretrained(output_dir)
 
